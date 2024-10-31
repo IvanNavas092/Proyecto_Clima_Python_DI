fe4404653bd43f83d24eeb69561a08e4bd565a71
IvanNavas092 <navass2111@gmail.com>
2024-10-31 13:32:10 +0100

    elimino config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 24 19:48:46 2024
-
-@author: navas
-"""
-#%% KEYS
-API_KEY = "2cd435f21fmsh1df1ea6a983a6f5p13e015jsnb972c8046f09"
-GEOCODE_API_KEY = "d3c7abe8ba18457b898e4b01593e3159"
